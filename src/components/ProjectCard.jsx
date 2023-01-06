a88e1af7395c34f3fa02a64a686dce123f11799d
Sulekha Jamaluddin <sulu.lekha@gmail.com>
2023-01-06 17:54:34 +0100

    Fix projects and technologies

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,30 +1,35 @@
 import ProjectDetails from "./ProjectDetails";
 
 export default function ProjectCard({ cssClass, setModal, project }) {
+  const { projectID, projectName, projectCardImage, imageCardDescription } =
+    project;
+
   const handleModal = (project) => {
     setModal(<ProjectDetails project={project} setModal={setModal} />);
   };
 
+  const overlay = (
+    <div className="overlay">
+      <p>COMING SOON...</p>
+    </div>
+  );
+
   return (
-    <div key={project.projectID}>
+    <div key={projectID}>
       <button
         className="project-card"
         disabled={`${cssClass}` === "comingSoon" ? true : false}
         onClick={() => handleModal(project)}
       >
-        {cssClass === "comingSoon" && (
-          <div className="overlay">
-            <p>COMING SOON...</p>
-          </div>
-        )}
+        {cssClass === "comingSoon" && overlay}
         <div className={`card ${cssClass}`}>
           <img
             className="card__image"
-            src={project.projectCardImage}
-            alt={project.imageCardDescription}
+            src={projectCardImage}
+            alt={imageCardDescription}
           />
           <div>
-            <p>{project.projectName}</p>
+            <p>{projectName}</p>
           </div>
         </div>
       </button>
