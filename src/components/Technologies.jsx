a511f4e1749fd4ceb5d0de080d444e3ab5dde976
Sulekha Jamaluddin <sulu.lekha@gmail.com>
2024-10-15 13:33:53 +0200

    Correct technologies description

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -15,10 +15,7 @@ export default function Technologies({ setModal }) {
       <div className="section-one">
         <div className="content">
           <h2>TECHNOLOGIES</h2>
-          <p>
-            Here are some technologies that I already know and some that I am
-            learning here at Novare.
-          </p>
+          <p>Here are some technologies that i know.</p>
         </div>
       </div>
       <div className="grid">{techList}</div>
